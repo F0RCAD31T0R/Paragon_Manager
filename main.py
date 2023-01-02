79c1d8490021f727ea6bc43d705b97f585f84f37
achoolucgust <76622722+achoolucgust@users.noreply.github.com>
2023-01-03 04:44:24 +0800

    main
    async def on_ready()

---
+++ main.py
@@ -0,0 +1,10 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Connected to discord")
