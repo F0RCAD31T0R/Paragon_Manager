04d4def95ac1742b3f429e13a29f62f252dd5907
achoolucgust <76622722+achoolucgust@users.noreply.github.com>
2023-01-04 13:30:30 +0800

    heawlth check

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Bot ONLINE! [Server is online, Idk how to make health checks yet.]"
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
 def getapp():
     return app
