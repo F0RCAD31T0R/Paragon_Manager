8bf3765fc4550a439e9b111c963652ec50d369f2
achoolucgust <76622722+achoolucgust@users.noreply.github.com>
2023-01-09 12:49:05 +0800

    status web

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+import requests
 import json
 
 app = Flask(__name__)
 
+online = open("./html/status/online.html").read()
+offline = open("./html/status/offline.html").read()
+
 @app.route('/')
 def index():
+    print(requests.get("https://paragon-manager-bot.onrender.com/status/").status_code)
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+@app.route('/status/')
+def status():
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
 
 def getapp():
     return app
