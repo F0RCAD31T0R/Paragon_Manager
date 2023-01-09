94d595d0d04c920f430fd7c9cbdafaafb447efde
achoolucgust <76622722+achoolucgust@users.noreply.github.com>
2023-01-10 01:03:19 +0800

    Automod fixed welcoming website now with CSS

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import nextcord
+from similarcharacters import Confusables
 from nextcord.ext import commands
-import threading
-from datetime import datetime
 from datetime import timezone
+import threading
+import nextcord
 import os
+import re
 
 activity = nextcord.Activity(name='Paragon Studios', type=nextcord.ActivityType.watching)
-bot = commands.Bot(activity=activity)
+intents = nextcord.Intents.default()
+intents.messages = True
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(activity=activity,intents=intents)
 
 def UTC_datetime_to_timestamp(time):
     return time.replace(tzinfo=timezone.utc).timestamp()
@@ -16,6 +22,8 @@ async def find_channel_by_name(name,category):
         if channel.name.lower() == name.lower():
             return channel
     return None
+
+c = Confusables("confusables.txt")
 
 class MainStuff(commands.Cog):
     def __init__(self,bot):
@@ -99,10 +107,45 @@ class Misc(commands.Cog):
     EMOJIS: {len(guild.emojis)}
     ```""")
 
+class Welcoming(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+    @bot.listen("on_member_join")
+    async def on_member_join(member):
+        channel = await bot.fetch_channel("1059947304326545458")
+        welcome_channel = await bot.fetch_channel("1047503199617171537")
+        await channel.edit(name=f"{channel.guild.member_count} Members")
+        await welcome_channel.send(f"WELCOME <@{member.id}> ({member.id})")
+        
+
+triggerfile = open("automodtriggers.txt","r")
+triggers = triggerfile.read().split("\n")
+regexTriggers = []
+for trigger in triggers:
+    triggerInRegex = re.compile(c.confusables_regex(trigger))
+    regexTriggers.append(triggerInRegex)
+
+class AutoModeration(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    #@commands.Cog.listener()
+    #async def on_member_join(self, member): # dont use this for now
+    @bot.listen('on_message')
+    async def on_message(message):
+        for regex in regexTriggers:
+            for word in message.content.split(" "):
+                if regex.match(word.lower()):
+                    await message.delete()
+
+        
+
 bot.add_cog(MainStuff(bot))
 bot.add_cog(TicketSystem(bot))
 bot.add_cog(HelpCommand(bot))
 bot.add_cog(Misc(bot))
+bot.add_cog(Welcoming(bot))
+bot.add_cog(AutoModeration(bot))
 
 def start_server():
     os.system("gunicorn -w 4 'app:app'")
